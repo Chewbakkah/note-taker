a999f20f8f8d29d72cc9fc8203dd54090ca40935
Chewbakkah <chewbakkah@gmail.com>
2022-01-28 00:21:17 -0600

    debuggifying

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const fs = require('fs');
-
+// npm package that assigns unique id's
 const { nanoid } = require('nanoid');
 const id = nanoid();
 
-let notes = [];
+let notes;
 
 // gets the notes data
 router.get('/notes', (req, res) => {
@@ -13,33 +13,33 @@ router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-// create new notes
+// create new entry
 router.post('/notes', (req, res) => {
     req.body.id = id;
 
-    // adds the new note to the notes json
-    let newNotes = req.body;
-    notes.push(newNotes);
+    // takes body and pushes to notes array
+    let incoming = req.body;
+    notes.push(incoming);
 
-    // writes the new note to the json file
+    // writes the new note to the db
     fs.writeFileSync('db/db.json', JSON.stringify(notes), 'utf8');
 
-    // returns the new note to the client, posting it on the side bar
+    // sends updated note list to user
     res.json(notes);
 });
 
-// delete note upon clicking the trash can icon
+// delete note 
 router.delete('/notes/:id', (req, res) => {
-    // get the note to delete by id
+    // get id from request
     const noteId = req.params.id;
-    
-    let notesRemain = notes.filter( currNote => {
-        return currNote.id != noteId;
+    //uses filter to create a new array with all the old info except the requested note with the incoming id
+    let notesSans = notes.filter( currentNote => {
+        return currentNote.id != noteId;
     })
 
     // writes the notes to the json file without the deleted note
-    fs.writeFileSync('db/db.json', JSON.stringify(notesRemain), 'utf8');
-    res.json(notesRemain);
+    fs.writeFileSync('db/db.json', JSON.stringify(notesSans), 'utf8');
+    res.json(notesSans);
 });
 
 module.exports = router;
